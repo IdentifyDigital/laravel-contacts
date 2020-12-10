5b526784b175f8fb85a80ee4086492b45a66c9a5
bradleyking-dev <57000861+bradleyking-dev@users.noreply.github.com>
2020-12-10 10:08:53 +0000

    Update Address.php
    Amended per review

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -33,13 +33,46 @@ class Address extends Model
         'relation_id'
     ];
 
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
     protected $appends = [
         'address_string'
     ];
 
+    /**
+     * Magic Method
+     * Gets a String of the full address
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getAddressStringAttribute();
+    }
+
+    /**
+     * Gets a String of the full address
+     *
+     * @return string
+     */
     public function getAddressStringAttribute()
     {
-        return "{$this->line_1}, {$this->line_2}, {$this->city}, {$this->postcode}";
+        $string = "";
+        $requiredAttributes = ['line_1', 'line_2', 'line_3', 'city', 'postcode'];
+        foreach ($requiredAttributes as $key => $attribute){
+            if ($key == 0) {
+                $string .= $this->{$attribute};
+            }
+            else {
+                if ($this->{$attribute} !== null) {
+                    $string .= ', ' . $this->{$attribute};
+                }
+            }
+        }
+        return $string;
     }
 
 }
